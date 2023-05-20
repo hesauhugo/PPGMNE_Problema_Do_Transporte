a1e6664b2680495e2a257bc40322ac1c1370f01c
hesauhugo <hesau.hugo@hotmail.com>
2023-05-20 09:47:08 -0300

    feat: upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace Aula2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProblemaTransporte = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ProblemaTransporte
+            // 
+            this.ProblemaTransporte.Location = new System.Drawing.Point(334, 85);
+            this.ProblemaTransporte.Name = "ProblemaTransporte";
+            this.ProblemaTransporte.Size = new System.Drawing.Size(75, 53);
+            this.ProblemaTransporte.TabIndex = 0;
+            this.ProblemaTransporte.Text = "Problema Transporte";
+            this.ProblemaTransporte.UseVisualStyleBackColor = true;
+            this.ProblemaTransporte.Click += new System.EventHandler(this.ProblemaTransporte_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Número de colunas";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(203, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Número de linhas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ProblemaTransporte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ProblemaTransporte;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gurobi;
+
+namespace Aula2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProblemaTransporte_Click(object sender, EventArgs e)
+        {
+            Random Aleatorio = new Random(1);
+            int NumOrigens = 50;
+            int NumDestinos = 50;
+            //int OfertaTotal = 75;
+            int[,] CustosVariaveis = new int[NumOrigens, NumDestinos];
+            int[,] CustosFixos = new int[NumOrigens, NumDestinos];
+            int[] Ofertas = new int[NumOrigens];
+            int[] Demandas = new int[NumDestinos];
+            int[,] MatrizM = new int[NumOrigens, NumDestinos];
+            GRBEnv Ambiente = new GRBEnv();
+            GRBModel Modelo = new GRBModel(Ambiente);
+            GRBVar[,] X = new GRBVar[NumOrigens, NumDestinos];
+            GRBVar[,] W = new GRBVar[NumOrigens, NumDestinos];
+
+            //Gerar custos aleatórios
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                for (int j = 0; j < NumDestinos; j++)
+                {
+                    CustosVariaveis[i, j] = Aleatorio.Next(3, 15);
+                    CustosFixos[i, j] = Aleatorio.Next(300, 501);
+                }
+            }
+
+            //Definir a oferta de cada origem
+            int Soma = 0;
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                Ofertas[i] = Aleatorio.Next(80, 121);
+                Soma += Ofertas[i];
+            }
+            //int Diferenca = OfertaTotal - Soma;
+            //if(Diferenca>0)
+            //{
+            // for (int d = 0; d < Diferenca; d++)
+            // {
+            // int OrigemEscolhida = Aleatorio.Next(0, NumOrigens);
+            // Ofertas[OrigemEscolhida] += 1;
+            // }
+            //}
+            //else if(Diferenca<0)
+            //{
+            // for(int d=0;d<Diferenca;d++)
+            // {
+            // int OrigemEscolhida = Aleatorio.Next(0, NumOrigens);
+            // while(Ofertas[OrigemEscolhida]<=1)
+            // {
+            // OrigemEscolhida = Aleatorio.Next(0, NumOrigens);
+            // }
+            // Ofertas[OrigemEscolhida] -= 1;
+            // }
+            //}
+
+            //Definir a demanda de cada destino
+            for (int j = 0; j < NumDestinos; j++)
+            {
+                Demandas[j] = Aleatorio.Next(80, 101);
+            }
+
+            //Criar Matriz M
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                for (int j = 0; j < NumDestinos; j++)
+                {
+                    if (Ofertas[i] <= Demandas[j])
+                    {
+                        MatrizM[i, j] = Ofertas[i];
+                    }
+                    else
+                    {
+                        MatrizM[i, j] = Demandas[j];
+                    }
+                }
+            }
+
+            //Definir as variáveis de decisão do modelo e a função objetivo
+            GRBLinExpr FuncaoObjetivo = new GRBLinExpr();
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                for (int j = 0; j < NumDestinos; j++)
+                {
+                    X[i, j] = Modelo.AddVar(0, double.MaxValue, 0, GRB.CONTINUOUS, $"x_{i}_{j}");
+                    W[i, j] = Modelo.AddVar(0, double.MaxValue, 0, GRB.BINARY, $"w_{i}_{j}");
+                }
+            }
+
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                for (int j = 0; j < NumDestinos; j++)
+                {
+                    FuncaoObjetivo.AddTerm(CustosVariaveis[i, j], X[i, j]);
+                    FuncaoObjetivo.AddTerm(CustosFixos[i, j], W[i, j]);
+                }
+            }
+            Modelo.SetObjective(FuncaoObjetivo);
+
+            //Criar as restrições de oferta
+            GRBLinExpr expr = new GRBLinExpr();
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                expr.Clear();
+                for (int j = 0; j < NumDestinos; j++)
+                {
+                    expr.AddTerm(1, X[i, j]);
+                }
+                Modelo.AddConstr(expr <= Ofertas[i], $"Of_{i}");
+            }
+
+            //Criar as restrições de demanda
+            for (int j = 0; j < NumDestinos; j++)
+            {
+                expr.Clear();
+                for (int i = 0; i < NumOrigens; i++)
+                {
+                    expr.AddTerm(1, X[i, j]);
+                }
+                Modelo.AddConstr(expr >= Demandas[j], $"De_{j}");
+            }
+
+            //Criar restrições que relacionam as variáveis X e W
+            for (int i = 0; i < NumOrigens; i++)
+            {
+                for (int j = 0; j < NumDestinos; j++)
+                {
+                    Modelo.AddConstr(X[i, j] <= MatrizM[i, j] * W[i, j], $"Hab_{i}_{j}");
+                }
+            }
+
+            //Escrever o modelo .lp, resolver o modelo e escrever a solução
+            Modelo.Write(@"ModeloTransporte.lp");
+            Modelo.Optimize();
+            Modelo.Write(@"SolucaoTransporte.sol");
+        }
+
+    }
+}
